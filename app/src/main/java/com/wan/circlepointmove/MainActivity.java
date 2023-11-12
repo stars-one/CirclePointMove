7c07ce5db2f2a74e7a157144e1ca45bc5059d58d
starone <starsone@Foxmail.com>
2023-11-12 16:34:52 +0800

    :recycle: 重构升级为androidx

--- app/src/main/java/com/wan/circlepointmove/MainActivity.java
+++ app/src/main/java/com/wan/circlepointmove/MainActivity.java
@@ -1,8 +1,8 @@
 package com.wan.circlepointmove;
 
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
--- app/src/main/java/com/wan/circlepointmove/MyViewPagerAdapter.java
+++ app/src/main/java/com/wan/circlepointmove/MyViewPagerAdapter.java
@@ -1,6 +1,6 @@
 package com.wan.circlepointmove;
 
-import android.support.v4.view.PagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
